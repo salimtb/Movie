910ba3ff1d0d8d65bf4c14045a9be387b71da881
salimtb <salimtbl@outlook.com>
2017-11-20 11:32:42 +0100

    création de la page login et registration pour l'application

---
+++ Easy/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Easy.Models;
+
+
+namespace Easy.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Autherize(Easy.Models.User userModel)
+        {
+            using (AppEntities db = new AppEntities())
+            {
+                var userDetails = db.User.Where(x => x.UserName == userModel.UserName && x.Password == userModel.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                   
+                    userModel.LoginErrorMessage = "Nom d'utilisateur ou mot de passe incorrecte ";
+                    return View("index", userModel);
+                }
+                else
+                {
+                    Session["userID"] = userDetails.UserID;
+                    Session["userName"] = userDetails.UserName;
+
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+        }
+        public ActionResult Logout()
+        {
+            int User1 = (int)Session["userID"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+
+        }
+    }
+}
---
+++ Easy/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Easy.Models;
+
+namespace Easy.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult Register(int id=0)
+        {
+            User userModel = new User();
+            return View(userModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User userModel)
+        {
+            using (AppEntities db = new AppEntities())
+            {
+
+
+                if (db.User.Any(x => x.UserName == userModel.UserName))
+                {
+                    ViewBag.DuplicateMessage = "Utilisateurs existant";
+                    return View("Register", userModel);
+                }
+                db.User.Add(userModel);
+
+                db.SaveChanges();
+
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Inscription réussi avec succès";
+
+            return View("Register", new User());
+
+
+        }
+    }
+}
